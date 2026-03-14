83616502bd70a5da412fcbb44aab6940d9142c95
hronchizhevoshice <hronchizhevoshice@gmail.com>
2026-03-14 14:30:45 +0700

    Create models movie

---
+++ app/src/main/java/com/example/wewatchmvc/model/Movie.kt
@@ -0,0 +1,14 @@
+package com.example.wewatchmvc.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "movies")
+data class Movie(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0,
+    val title: String,
+    val year: String,
+    val posterUrl: String,
+    val imdbID: String
+)
